e8bee5a141249cf24537f1ad12a4a35f87ba1750
Riitha <myuuta29.00@gmail.com>
2025-07-19 12:02:21 +0700

    style:menambahkan responsive img pada samping form login page dan register page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase";
+import loginGunpla from "../assets/gunplaLogin.png";
+import loginPilot from "../assets/pilotLogin.png";
+import halo from "../assets/haro.png";
+import { Link } from "react-router";
 
 export default function LoginPage() {
     //useState
@@ -21,31 +25,46 @@ export default function LoginPage() {
 
     return (
         <>
-            <div className="hero relative min-h-screen bg-cover bg-[url('./assets/bgLogin.png')]">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-3xl font-['WDXL_Lubrifont_JP_N'] whitespace-nowrap md:text-5xl">アクセス認証</h1>
-                        <p className="py-6 font-['Zen_Maru_Gothic']">
-                            認証情報を入力してください
-                        </p>
+            <div className="hero relative min-h-screen bg-cover bg-desert-storm">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-12">
+                    <div className="container bg-linear-to-b from-aquaDeep to-turquoise lg:bg-linear-to-b lg:from-cello lg:to-turquoise mx-auto w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl rounded-2xl overflow-hidden">
+
+                        <img
+                            src={loginPilot}
+                            alt="Pilot"
+                            className="block lg:hidden w-full h-full object-contain"
+                        />
+
+                        <img
+                            src={loginGunpla}
+                            alt="Gunpla"
+                            className="hidden lg:block w-full h-full object-contain"
+                        />
+
                     </div>
-                    <div className="card w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className="card w-full max-w-sm shrink-0 overflow-hidden">
                         <form onSubmit={handleLogin}>
-                            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4 shadow-2xl">
-                                <legend className="fieldset-legend">ログイン</legend>
+                            <fieldset className="fieldset bg-blue-300 border-base-300 rounded-box w-full border p-4">
+                                <legend className="fieldset-legend text-cello text-shadow-xl"><img src={halo} alt="mascot" className="w-[30px]"/></legend>
 
-                                <label className="label">メール</label>
-                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="input" placeholder="Email" />
+                                <label className="label text-cello ml-4">メール</label>
+                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="input bg-white/30 mx-auto text-black/80 placeholder-cello" placeholder="Email" />
 
-                                <label className="label">パスワード</label>
-                                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="input" placeholder="Password" />
+                                <label className="label text-cello ml-4">パスワード</label>
+                                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" className="input binput bg-white/30 mx-auto input bg-white/30 mx-auto text-black/80 placeholder-cello" placeholder="Password" />
 
-                                <button type="submit" className="btn btn-neutral mt-4">ログイン</button>
+                                <button type="submit" className="btn btn-accent mx-auto">ログイン</button>
                             </fieldset>
                         </form>
+                        <p className="text-sm text-gray-600 mt-4">
+                            未登録は{' '}
+                            <Link to='/auth/register' className="text-ship hover:underline">
+                            こちらへ
+                            </Link>
+                        </p>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router";
 import { auth } from "../config/firebase";
+import gunpla from "../assets/gunplaRegister.png";
+import pilot from "../assets/pilotRegister.png";
+import halo from "../assets/haro.png";
+import { Link } from "react-router";
+
 export default function RegisterPage() {
     //useState
     const [email, setEmail] = useState("");
@@ -20,28 +25,42 @@ export default function RegisterPage() {
     }
     return (
         <>
-            <div className="hero min-h-screen bg-cover bg-[url('./assets/bgRegister.png')] bg-center bg-no-repeat relative">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-3xl font-['WDXL_Lubrifont_JP_N'] whitespace-nowrap md:text-5xl">ガンプライムへようこそ！</h1>
-                        <p className="py-6 font-['Zen_Maru_Gothic'] text-xl md:text-2xl">
-                            数多な<span className="text-sunblow">世界</span>があなたを待っている
-                        </p>
+            <div className="hero relative min-h-screen bg-cover bg-desert-storm">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-12">
+                    <div className="container bg-linear-to-b from-malibu to-midnight-blue lg:bg-linear-to-b lg:from-firefly lg:to-turquoise mx-auto w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl rounded-2xl overflow-hidden">
+                        <img
+                            src={pilot}
+                            alt="Pilot"
+                            className="block lg:hidden w-full h-full object-contain"
+                        />
+
+                        <img
+                            src={gunpla}
+                            alt="Gunpla"
+                            className="hidden lg:block w-full h-full object-contain"
+                        />
                     </div>
-                    <div className="card w-full max-w-sm shrink-0 shadow-2xl">
+
+                    <div className="card w-full max-w-sm shrink-0 overflow-hidden">
                         <form onSubmit={handleRegister}>
-                            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                                <legend className="fieldset-legend">登録</legend>
+                            <fieldset className="fieldset bg-blue-300 border-base-300 rounded-box w-full border p-4">
+                                <legend className="fieldset-legend text-cello text-shadow-xl"><img src={halo} alt="mascot" className="w-[30px]" /></legend>
 
-                                <label className="label">メール</label>
-                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input" placeholder="Email" />
+                                <label className="label text-cello ml-4">メール</label>
+                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input bg-white/30 mx-auto text-black/80 placeholder-cello" placeholder="Email" />
 
-                                <label className="label">パスワード</label>
-                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" />
+                                <label className="label text-cello ml-4">パスワード</label>
+                                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input binput bg-white/30 mx-auto input bg-white/30 mx-auto text-black/80 placeholder-cello" placeholder="Password" />
 
-                                <button type="submit" className="btn btn-neutral mt-4">登録</button>
+                                <button type="submit" className="btn btn-accent mx-auto">登録</button>
                             </fieldset>
                         </form>
+                        <p className="text-sm text-gray-600 mt-4">
+                            登録済みユーザーは{' '}
+                            <Link to='/auth/login' className="text-ship hover:underline">
+                                ログイン
+                            </Link>
+                        </p>
                     </div>
                 </div>
             </div>
